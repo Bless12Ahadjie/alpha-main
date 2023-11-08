83e0c78b2739d86a6d3d32bcf38f34bd354543cf
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-11-08 10:15:21 +0000

    changes

--- pfa/src/molecules/Body.jsx
+++ pfa/src/molecules/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../css modules/Body.module.css';
+import OverviewBox from '../organisms/OverviewBox';
 
 
 const Body = () => {
     return (
         <div className={styles.container}>
-           
+           <OverviewBox />
         </div>
     );
 }
---
+++ pfa/src/organisms/OverviewBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../css modules/OvervieBox.module.css'
+
+const OverviewBox = ({style}) => {
+    return (
+        <div style={style} className={styles.Box}>
+          <div style={style} className={styles.headBox}>
+
+          </div>
+        </div>
+    );
+}
+
+export default OverviewBox;

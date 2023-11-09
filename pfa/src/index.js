16219721de00500ae4a2f5755fbcc00b721cb41a
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-11-09 16:42:38 +0000

    chages

--- pfa/src/index.js
+++ pfa/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { MantineProvider, createTheme } from '@mantine/core';
 import reportWebVitals from './reportWebVitals';
 
 // routing
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <MantineProvider >
       <App />
-    </MantineProvider>
    </BrowserRouter>
   </React.StrictMode>
  
